f53bc39ef7f1daec0df0ff4349d65c726642146f
Anmol Angad Singh <anmolangadsingh11@gmail.com>
2020-11-12 19:20:06 +0530

    Problem347 - Top K Frequent Elements Header comments.
    Problem3 - Longest Substring Without Repeating Characters
    Complete solution.

---
+++ src/com/leetcode/algorithms/medium/Problem3.java
@@ -0,0 +1,37 @@
+package com.leetcode.algorithms.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * 3. Longest Substring Without Repeating Characters
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * Tags: Pointers, Two Pointers, Array, Sliding Window
+ * */
+
+public class Problem3 {
+
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        Set<Character> set = new HashSet<>();
+        int maxLength = 0, indexLeft = 0, indexRight = 0;
+        while(indexRight < s.length()){
+            if(!set.contains(s.charAt(indexRight))){
+                set.add(s.charAt(indexRight));
+                indexRight++;
+                maxLength = Math.max(maxLength, set.size());
+            }else{
+                set.remove(s.charAt(indexLeft));
+                indexLeft++;
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Problem3 problem3 = new Problem3();
+        System.out.println(problem3.lengthOfLongestSubstring("abcabablubcbb"));
+    }
+}
--- src/com/leetcode/algorithms/medium/Problem347.java
+++ src/com/leetcode/algorithms/medium/Problem347.java
@@ -1,5 +1,11 @@
 package com.leetcode.algorithms.medium;
 
+/*
+ * 347. Top K Frequent Elements
+ * https://leetcode.com/problems/top-k-frequent-elements/
+ * Tags: Priority Queue, Heaps
+ * */
+
 public class Problem347 {
 
     public static void main(String[] args) {
