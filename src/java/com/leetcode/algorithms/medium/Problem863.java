75fb6acacc40661b3ff1edf13b8079943f630351
Anmol Angad Singh <anmolangadsingh11@gmail.com>
2020-11-18 23:03:37 +0530

    Problem863 initial commit

---
+++ src/java/com/leetcode/algorithms/medium/Problem863.java
@@ -0,0 +1,25 @@
+package com.leetcode.algorithms.medium;
+
+import java.util.List;
+
+public class Problem863 {
+
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+    return null;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
