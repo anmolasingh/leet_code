38b76ddecbbfc21793371e2f0b6976e787a2d9ff
Anmol Angad Singh <anmolangadsingh11@gmail.com>
2020-11-25 16:03:34 +0530

    Complete solution - 2 approaches

--- src/java/com/leetcode/algorithms/medium/Problem236.java
+++ src/java/com/leetcode/algorithms/medium/Problem236.java
@@ -6,6 +6,8 @@ package com.leetcode.algorithms.medium;
  * Tags: Tree, Binary Tree
  * */
 
+import java.util.List;
+
 public class Problem236 {
 
   static class TreeNode {
@@ -19,8 +21,85 @@ public class Problem236 {
     }
   }
 
+  //Recursive approach
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    return checkNode(root, p, q);
+  }
+
+  private TreeNode checkNode(TreeNode node, TreeNode p, TreeNode q) {
+    if(node == null){
+      return null;
+    }
+
+    if(node.val == p.val || node.val == q.val){
+      return node;
+    }
+
+    TreeNode left = checkNode(node.left, p, q);
+    TreeNode right = checkNode(node.right, p, q);
+    if(left!= null && right != null){
+      return node;
+    }else if(left != null){
+      return  left;
+    }else if(right != null){
+      return right;
+    }
     return null;
+  }
+
+  //Array approach
+  /*public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    List<TreeNode> arr1 = new ArrayList<>();
+    List<TreeNode> arr2 = new ArrayList<>();
+    hasPath(root, arr1, p.val);
+    hasPath(root, arr2, q.val);
+
+    int index1 = 0, index2 = 0;
+    TreeNode result = null;
+    while (index1 < arr1.size() && index2 < arr2.size()){
+      TreeNode node1 = arr1.get(index1);
+      TreeNode node2 = arr2.get(index2);
+      if(node1.val == node2.val){
+        result = node1;
+      }
+      index1++;
+      index2++;
+    }
+
+    return result;
+  }*/
+
+  public static boolean hasPath(TreeNode node, List<TreeNode> arr, int x) {
+    // if root is NULL
+    // there is no path
+    if (node == null) {
+      return false;
+    }
+
+    // push the node's value in 'arr'
+    arr.add(node);
+
+    // if it is the required node
+    // return true
+    if (node.val == x) {
+      return true;
+    }
+
+    // else check whether the required node lies
+    // in the left subtree or right subtree of
+    // the current node
+    if (hasPath(node.left, arr, x) ||
+        hasPath(node.right, arr, x)) {
+      return true;
+    }
+
+    // required node does not lie either in the
+    // left or right subtree of the current node
+    // Thus, remove current node's value from
+    // 'arr' and then return false
+    arr.remove(arr.size()-1);
+    return false;
   }
 
   public static void main(String[] args) {
@@ -34,7 +113,7 @@ public class Problem236 {
     root.left.right.left = new Problem236.TreeNode(7);
     root.left.right.right = new Problem236.TreeNode(4);
     System.out
-        .println(new Problem236().lowestCommonAncestor(root, root.left.left, root.left.right));
+        .println(new Problem236().lowestCommonAncestor(root, root.left, root.left.right.right).val);
   }
 
 }
